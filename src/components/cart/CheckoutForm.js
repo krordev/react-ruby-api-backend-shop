d9bbdbcbd071abe2b4d34b58a15f986f27883e8b
krordev <kelly.knightn3@gmail.com>
2021-05-02 12:23:59 -0400

    basic event handlers for checkout form

--- src/components/cart/CheckoutForm.js
+++ src/components/cart/CheckoutForm.js
@@ -17,30 +17,37 @@ class CheckoutForm extends Component {
         })
     }
 
+    handleChange = e => this.setState({ [e.target.name]: e.target.value })
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={(e) => this.handleSubmit(e)}>
                     <br></br>
                     <label>Please Enter Checkout Details</label><br></br>
                     
-                    <label htmlFor="name">Name:</label>
-                    <input id="name" type="text" name="name" /><br></br>
+                    <label htmlFor="customerName">Name:</label>
+                    <input id="customerName" type="text" name="customerName" onChange={this.handleChange} /><br></br>
 
                     <label htmlFor="address">Address:</label>
-                    <textarea id="address" type="textarea" name="address" /><br></br>
+                    <textarea id="address" type="textarea" name="address" onChange={this.handleChange} /><br></br>
 
                     <label htmlFor="cardnum">Card Number:</label>
-                    <input id="cardnum" type="text" name="cardnum" /><br></br>
+                    <input id="cardnum" type="text" name="cardNumber" onChange={this.handleChange} /><br></br>
 
                     <label htmlFor="cardexp">Expiration Date (MM/YY):</label>
-                    <input id="cardexp" type="text" name="cardexp" /><br></br>
+                    <input id="cardexp" type="text" name="cardExp" onChange={this.handleChange} /><br></br>
 
                     <label htmlFor="cardsec">Security Code:</label>
-                    <input id="cardsec" type="text" name="cardsec" /><br></br>
+                    <input id="cardsec" type="text" name="cardSecurityNum" onChange={this.handleChange} /><br></br>
 
                     <label htmlFor="note">Customer Note:</label>
-                    <input id="note" type="text" name="note" /><br></br>
+                    <input id="note" type="text" name="note" onChange={this.handleChange} /><br></br>
 
                     <input type="Submit" value="Submit Order" />
 
