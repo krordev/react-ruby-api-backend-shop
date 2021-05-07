ed76fb701d266f92401380fbe38d42bf4504f903
krordev <kelly.knightn3@gmail.com>
2021-05-07 11:46:01 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const store = createStore(rootReducer,
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store} >
       <App />
     </Provider >
   </React.StrictMode>,
